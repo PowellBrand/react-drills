7bbf585e3a96bde883a40c50c6e650658879af91
PowellBrand <powellbrandon@outlook.com>
2017-12-04 14:50:48 -0700

    app3 done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+constructor(){
+  super()
+
+  this.state={
+    filterBox:'',
+    arrayItems:['brandon','amber','david','lisa']
+  }
+}
+
+handleChange(value){
+this.setState({
+  filterBox: value
+})
+}
+
+
+
   render() {
+    var stuffToDisplay = this.state.arrayItems.filter( (element, index) => {
+      return element.includes( this.state.filterBox );
+    }).map( (element, index) => {
+      return <h1 key={ index }>{ element }</h1>
+
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App">    
+        <input onChange={ (e) => this.handleChange( e.target.value ) } type="text" /> 
+        {stuffToDisplay}     
       </div>
     );
   }
